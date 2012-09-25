f23888cfb91331d25180269537eb1166c3f3026c
meladawy <magedbik@gmail.com>
2012-09-25 16:11:07 +0200

    Update Read me , Add file .php

--- README.txt
+++ README.txt
@@ -1,2 +1,19 @@
-Hello this is my first Readme
-Update...
+commit 5d8cb861dbd3139d62fb5f84bb21022ea6cfd96f
+Author: meladawy <magedbik@gmail.com>
+Date:   Tue Sep 25 15:18:03 2012 +0200
+
+    Add file.php file
+
+diff --git a/file.php b/file.php
+new file mode 100644
+index 0000000..5a25157
+--- /dev/null
++++ b/file.php
+@@ -0,0 +1,7 @@
++<?php 
++
++echo "Hello world" ; 
++
++function magedbik(){
++ echo "This is function" ; 
++}
--- file.php
+++ file.php
@@ -1,7 +1,19 @@
-<?php 
+commit 5d8cb861dbd3139d62fb5f84bb21022ea6cfd96f
+Author: meladawy <magedbik@gmail.com>
+Date:   Tue Sep 25 15:18:03 2012 +0200
 
-echo "Hello world" ; 
+    Add file.php file
 
-function magedbik(){
- echo "This is function" ; 
-}
+diff --git a/file.php b/file.php
+new file mode 100644
+index 0000000..5a25157
+--- /dev/null
++++ b/file.php
+@@ -0,0 +1,7 @@
++<?php 
++
++echo "Hello world" ; 
++
++function magedbik(){
++ echo "This is function" ; 
++}
